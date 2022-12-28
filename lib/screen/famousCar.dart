68e6ab6cb933f151907e0427a618b6a0090c758c
md7o <md7ohe@gmail.com>
2022-12-28 14:33:30 +0300

    last edit on famous page add person icon and name numbering

--- lib/screen/famousCar.dart
+++ lib/screen/famousCar.dart
@@ -8,54 +8,54 @@ class FamousCar extends StatelessWidget {
   Widget build(BuildContext context) {
     final List<popularsCar> popular = [
       const popularsCar(
-        title: 'Toyota Corolla',
+        title: '1- Toyota Corolla',
         backgroundImg: "images/Corolla.png",
-        selling: '1.1M',
+        selling: '1.1M 👤',
       ),
       const popularsCar(
-        title: 'Toyota RAV4',
+        title: '2- Toyota RAV4',
         backgroundImg: "images/RAV4.png",
-        selling: '1M',
+        selling: '1M 👤',
       ),
       const popularsCar(
-        title: 'Ford F-Series',
+        title: '3- Ford F-Series',
         backgroundImg: "images/Ford_F.png",
-        selling: '860K',
+        selling: '860K 👤',
       ),
       const popularsCar(
-        title: 'Honda CR-V',
+        title: '4- Honda CR-V',
         backgroundImg: "images/CRV.png",
-        selling: '730K',
+        selling: '730K 👤',
       ),
       const popularsCar(
-        title: 'Toyota Camry',
+        title: '5- Toyota Camry',
         backgroundImg: "images/Camry.png",
-        selling: '690K',
+        selling: '690K 👤',
       ),
       const popularsCar(
-        title: 'Ram Pick-up',
+        title: '6- Ram Pick-up',
         backgroundImg: "images/Ram.png",
-        selling: '650K',
+        selling: '650K 👤',
       ),
       const popularsCar(
-        title: 'Toyota Yaris',
+        title: '7- Toyota Yaris',
         backgroundImg: "images/Yaris.png",
-        selling: '590K',
+        selling: '590K 👤',
       ),
       const popularsCar(
-        title: 'Honda Civic',
+        title: '8- Honda Civic',
         backgroundImg: "images/Civic.png",
-        selling: '590K',
+        selling: '590K 👤',
       ),
       const popularsCar(
-        title: 'Chevrolet Silverado',
+        title: '9- Chevrolet Silverado',
         backgroundImg: "images/Silverado.png",
-        selling: '580K',
+        selling: '580K 👤',
       ),
       const popularsCar(
-        title: 'Tesla Model 3',
+        title: '10- Tesla Model 3',
         backgroundImg: "images/Tesla.png",
-        selling: '580K',
+        selling: '580K 👤',
       ),
     ];
 
@@ -75,7 +75,7 @@ class FamousCar extends StatelessWidget {
             children: const [
               Padding(
                 padding:
-                    EdgeInsets.only(top: 80, bottom: 40, left: 30, right: 30),
+                    EdgeInsets.only(top: 60, bottom: 30, left: 30, right: 30),
                 child: Text('Best selling cars worldwide in (2021)',
                     style: TextStyle(
                       color: Colors.white,
@@ -89,88 +89,90 @@ class FamousCar extends StatelessWidget {
 
           Expanded(
             child: ListView(
+                physics: const BouncingScrollPhysics(),
                 children: popular.map(
-              (po) {
-                return Padding(
-                  padding:
-                      const EdgeInsets.only(right: 20, left: 20, bottom: 50),
-                  child: Stack(
-                    children: [
-                      Positioned.fill(
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child:
-                              Image.asset(po.backgroundImg, fit: BoxFit.cover),
-                        ),
-                      ),
-                      Container(
-                        height: 200,
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          borderRadius: const BorderRadius.only(
-                            bottomLeft: Radius.circular(10),
-                            bottomRight: Radius.circular(10),
+                  (po) {
+                    return Padding(
+                      padding: const EdgeInsets.only(
+                          right: 20, left: 20, bottom: 50),
+                      child: Stack(
+                        children: [
+                          Positioned.fill(
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.asset(po.backgroundImg,
+                                  fit: BoxFit.cover),
+                            ),
                           ),
-                          gradient: LinearGradient(
-                            begin: Alignment.bottomCenter,
-                            end: Alignment.topCenter,
-                            colors: [
-                              Colors.black.withOpacity(0.7),
-                              Colors.transparent
-                            ],
-                          ),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(10),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    vertical: 10, horizontal: 10),
-                                child: Text(
-                                  po.title,
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                  textAlign: TextAlign.left,
-                                ),
+                          Container(
+                            height: 200,
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              borderRadius: const BorderRadius.only(
+                                bottomLeft: Radius.circular(10),
+                                bottomRight: Radius.circular(10),
                               ),
-                              Row(
+                              gradient: LinearGradient(
+                                begin: Alignment.bottomCenter,
+                                end: Alignment.topCenter,
+                                colors: [
+                                  Colors.black.withOpacity(0.7),
+                                  Colors.transparent
+                                ],
+                              ),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(10),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  Container(
-                                    width: 200,
-                                    height: 20,
-                                    decoration: BoxDecoration(
-                                        borderRadius:
-                                            (BorderRadius.circular(10)),
-                                        color: Colors.green.shade500),
-                                  ),
                                   Padding(
-                                    padding: const EdgeInsets.only(left: 10),
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 10, horizontal: 10),
                                     child: Text(
-                                      po.selling,
+                                      po.title,
                                       style: const TextStyle(
                                         color: Colors.white,
-                                        fontSize: 20,
+                                        fontSize: 16,
                                         fontWeight: FontWeight.bold,
                                       ),
+                                      textAlign: TextAlign.left,
                                     ),
                                   ),
+                                  Row(
+                                    children: [
+                                      Container(
+                                        width: 200,
+                                        height: 20,
+                                        decoration: BoxDecoration(
+                                            borderRadius:
+                                                (BorderRadius.circular(10)),
+                                            color: Colors.green.shade500),
+                                      ),
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 10),
+                                        child: Text(
+                                          po.selling,
+                                          style: const TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  )
                                 ],
-                              )
-                            ],
+                              ),
+                            ),
                           ),
-                        ),
+                        ],
                       ),
-                    ],
-                  ),
-                );
-              },
-            ).toList()),
+                    );
+                  },
+                ).toList()),
           ),
 
           // country
